fbf2a111d72f61cd9ed548815458e89f8613e177
ohnihili <bpfsignorelli@gmail.com>
2023-10-02 00:09:48 +1030

    some more

--- Abs.asm
+++ Abs.asm
@@ -2,3 +2,26 @@
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
 // Put your code here.
+
+    @1
+    D=M
+    @0
+    M=D
+    @abs
+    M=D
+    @END
+    D;JGE
+
+(NEG)
+    @abs
+    M=!M
+    M=M+1
+    D=M
+    @0
+    M=D
+    @END
+	0;JMP
+
+(END)  
+       @END
+       0; JMP
--- AddSub.asm
+++ AddSub.asm
@@ -2,3 +2,20 @@
 // (R0, R1, R2, R3 refer to RAM[0], RAM[1], RAM[2], and RAM[3], respectively.)
 
 // Put your code here.
+
+    @1
+    D=M
+    @2
+    M=D+M
+    D=M
+    @3
+    M=D-M
+    D=M
+    @0
+    M=D
+    @END
+	0;JMP
+
+(END)  
+       @END
+       0; JMP
