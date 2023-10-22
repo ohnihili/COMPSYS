e155c88914d70f649fc699b8ca08f00996c2e91c
ohnihili <bpfsignorelli@gmail.com>
2023-10-22 18:53:48 +1030

    fds

--- VMTranslator/VMTranslator.cpp
+++ VMTranslator/VMTranslator.cpp
@@ -127,18 +127,18 @@ string VMTranslator::vm_not() {
 }
 
 /** Generate Hack Assembly code for a VM label operation */
-string VMTranslator::vm_label(string label){
-    return "";
+string VMTranslator::vm_label(string label) {
+    return "(" + label + ")\n";
 }
 
 /** Generate Hack Assembly code for a VM goto operation */
-string VMTranslator::vm_goto(string label){
-    return "";
+string VMTranslator::vm_goto(string label) {
+    return "@" + label + "\n0;JMP\n";
 }
 
 /** Generate Hack Assembly code for a VM if-goto operation */
-string VMTranslator::vm_if(string label){
-    return "";
+string VMTranslator::vm_if(string label) {
+    return "@SP\nM=M-1\nA=M\nD=M\n@" + label + "\nD;JNE\n";
 }
 
 /** Generate Hack Assembly code for a VM function operation */
